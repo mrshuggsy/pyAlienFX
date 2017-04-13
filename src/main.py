b829b23c05101730d6a1d0098f007427a54e1834
MrShuggsy <shuggernaut@gmail.com>
2017-04-12 20:54:24 -0400

    testing vscode

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 """This is a python implementation of control over the AlienFX colored LEDs on Alienware Laptops"""
 
+GLOBAL_VAR2 = 4
+
+def my_func(myvar):
+    """Returns myvar+3
+
+    @param: myvar: some number
+    @returns: myvar + 3"""
+    return myvar + 3
 
 print("Hello World!!")
+
+print(my_func(GLOBAL_VAR2))
